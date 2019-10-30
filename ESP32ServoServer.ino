7652844a7401c0df8a5d6a0f0e6594cebad8bbfa
Kevin Harrington <mad.hephaestus@gmail.com>
2019-10-30 16:59:17 -0400

    Update ESP32ServoServer.ino

--- ESP32ServoServer.ino
+++ ESP32ServoServer.ino
@@ -5,6 +5,7 @@
 #include <PacketEvent.h>
 #include <Esp32SimplePacketComs.h>
 #include <Preferences.h>
+#include <Esp32WiFiManager.h>
 #include <wifi/WifiManager.h>
 #include <server/NameCheckerServer.h>
 #include "src/ServoServer.h"
